7c4f503450bc7bbc5dc0f4108ed25aa495d3e9d3
Flotter Totte <flottertotte@emeneo.com>
2025-03-20 20:01:31 +0100

    major lib change
    major lib change for better speed

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025030111;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025030112;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022111800;        // Requires this Moodle version.
 $plugin->component = 'local_courselist';  // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "Courselist plugin Version 4.5-d";
+$plugin->release   = "Courselist plugin Version 4.5-e";
