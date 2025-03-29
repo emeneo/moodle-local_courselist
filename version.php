2d5ec02ac41f8934465b05c9ccf473164b084987
Flotter Totte <flottertotte@emeneo.com>
2025-03-29 14:37:10 +0100

    Update version.php

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025030112;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025032900;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022111800;        // Requires this Moodle version.
 $plugin->component = 'local_courselist';  // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "Courselist plugin Version 4.5-e";
+$plugin->release   = "Courselist plugin Version 4.5-f";
